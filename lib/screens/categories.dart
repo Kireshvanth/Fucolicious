8a1f0cc45a9a19c6412e7b84ef25784a0d8c2fb3
Kireshvanth <kiresh20122002@gmail.com>
2021-06-13 12:34:17 +0530

    Making changes in the category screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_swipable/flutter_swipable.dart';
 
@@ -16,138 +17,155 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
+          //Container1 Starbucks logo,Starbucks watermark
           Container(
-            // width: screenWidth,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                // SizedBox(
-                //   height: 48,
-                // ),
                 Expanded(
-                  child: Container(),
-                  flex: 1,
-                ),
-                Image(
-                  image: AssetImage(
-                      "assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png"),
+                  flex: 30,
+                  child: Image.asset(
+                      'assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png'),
                 ),
                 Expanded(
-                  flex: 8,
-                  child: Container(),
+                  flex: 40,
+                  child: SizedBox(),
                 ),
-                // SizedBox(height: 474.75), //374.75
-                Image(
-                  image: AssetImage(
-                      "assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png"),
+                Expanded(
+                  child: Image.asset(
+                      'assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png'),
+                  flex: 30,
                 ),
               ],
             ),
           ),
-          Positioned(
-            top: 251.17,
-            left: 40.87,
-            child: Stack(
+          //Container2 Card watermark,swipeDown text,downArrow Icon
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Image(
-                    image: AssetImage(
-                        "assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png")),
-                Positioned(
-                  top: 16.83,
-                  left: 27.13,
+                Expanded(
+                  flex: 30,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 47,
+                  child: Container(
+                    margin: EdgeInsets.only(left: screenWidth / 10),
+                    child: Image.asset(
+                        'assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png'),
+                  ),
+                ),
+                Expanded(
+                  flex: 16,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 7,
                   child: Column(
                     children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          'Swipe Down',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Comfortaa',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Icon(
+                          Icons.expand_more,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //Container3 Card of that category,Selectcategory text ,coffee text , drawn path
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 12,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 18,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: screenWidth / 25),
+                        child: Text(
+                          'Select Category',
+                          style: TextStyle(
+                            fontFamily: 'Kenyan Coffee',
+                            fontStyle: FontStyle.italic,
+                            fontSize: screenHeight / 25,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 1
+                              ..color = Color(0xffFFF2D2),
+                          ),
+                        ),
+                      ),
                       Container(
-                        width: 322,
-                        height: 444,
                         child: Stack(
                           children: [
-                            Swipable(
-                              child: Image(
-                                  image: AssetImage(
-                                      "assests/categories_screen/Coffee_Card/Coffee_Card.png")),
+                            Container(
+                              child: Padding(
+                                padding:
+                                    EdgeInsets.only(left: screenWidth / 35),
+                                child: Container(
+                                  child: Image.asset(
+                                      'assests/categories_screen/Drawn_Path/Drawn_Path.png'),
+                                  height: screenHeight / 11,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: screenWidth / 15,
+                                    top: screenHeight / 75),
+                                child: Text(
+                                  'COFFEE',
+                                  style: TextStyle(
+                                    color: Color(0xffF4EFE3),
+                                    fontFamily: 'Antipasto Pro',
+                                    fontSize: screenHeight / 13,
+                                  ),
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ),
-                      Image(
-                          image: AssetImage(
-                              "assests/categories_screen/Dock/Dock.png")),
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.69, top: 159.69),
-            child: Image(
-                image: AssetImage(
-                    "assests/categories_screen/Drawn_Path/Drawn_Path.png")),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(15, 131, 15, 11),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Select Category",
-                      style: TextStyle(
-                          fontFamily: "Kenyan Coffee",
-                          fontSize: 29,
-                          shadows: [
-                            Shadow(
-                              // bottomLeft
-                              offset: Offset(-0.7, -0.7),
-                              color: Color.fromRGBO(255, 242, 210, 1),
-                            ),
-                            Shadow(
-                              // bottomRight
-                              offset: Offset(0.7, -0.7),
-                              color: Color.fromRGBO(255, 242, 210, 1),
-                            ),
-                            Shadow(
-                              // topRight
-                              offset: Offset(0.7, 0.7),
-                              color: Color.fromRGBO(255, 242, 210, 1),
-                            ),
-                            Shadow(
-                              // topLeft
-                              offset: Offset(-0.7, 0.7),
-                              color: Color.fromRGBO(255, 242, 210, 1),
-                            ),
-                          ]),
-                    ),
-                    Text(
-                      "COFFEE",
-                      style: TextStyle(
-                        fontFamily: "Antipasto Pro",
-                        fontSize: 62,
-                        color: Color.fromRGBO(244, 239, 227, 1),
-                      ),
-                    ),
-                  ],
                 ),
-                Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Swipe Down",
-                        style: TextStyle(
-                          fontFamily: "Comfortaa",
-                          fontSize: 12,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Image(
-                        image: AssetImage(
-                            "assests/categories_screen/Down_Arrow/Down_Arrow.png"),
-                      ),
-                    ],
+                Expanded(
+                  flex: 46,
+                  child: Swipable(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          left: screenWidth / 4, top: screenHeight / 100),
+                      child: Image.asset(
+                          'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                    ),
                   ),
+                ),
+                Expanded(
+                  flex: 24,
+                  child: SizedBox(),
                 ),
               ],
             ),
