abb2f501894e21463e0c9631ebe6a6e13cf650fb
Kireshvanth <kiresh20122002@gmail.com>
2021-06-14 18:47:07 +0530

    made a change in class for editing the categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -76,21 +76,6 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                                   image: AssetImage(
                                       "assests/categories_screen/Coffee_Card/Coffee_Card.png"),
                                 ),
-                                // child: Carousel(
-                                //   indicatorBgPadding: 2.0,
-                                //   autoplay: false,
-                                //   dotBgColor: Colors.transparent,
-                                //   boxFit: BoxFit.fitHeight,
-                                //   dotIncreasedColor: Color(0xffFFB500),
-                                //   images: [
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //   ],
-                                // ),
                               ),
                             ],
                           ),
@@ -119,194 +104,8 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                   ),
                 ],
               ),
-              Stack(
-                children: [
-                  //Starbucks Logo, Starbucks Watermark
-                  Container(
-                    // width: screenWidth,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          //-------Empty Space-----------
-                          child: Container(),
-                          flex: 1,
-                        ),
-                        //-------Starbucks Logo--------
-                        Image(
-                          image: AssetImage(
-                              "assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png"),
-                        ),
-                        //-------Empty Space-----------
-                        Expanded(
-                          flex: 8,
-                          child: Container(),
-                        ),
-                        //-------Starbucks Watermark--------
-                        Image(
-                          image: AssetImage(
-                              "assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  //Coffee Card, Coffee Card Watermark
-                  Positioned(
-                    top: 251.17,
-                    left: 40.87,
-                    child: Stack(
-                      children: [
-                        Image(
-                            image: AssetImage(
-                                "assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png")),
-                        Positioned(
-                          top: 16.83,
-                          left: 27.13,
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 322,
-                                height: 444,
-                                child: Image(
-                                  image: AssetImage(
-                                      "assests/categories_screen/Coffee_Card/Coffee_Card.png"),
-                                ),
-                                // child: Carousel(
-                                //   indicatorBgPadding: 2.0,
-                                //   autoplay: false,
-                                //   dotBgColor: Colors.transparent,
-                                //   boxFit: BoxFit.fitHeight,
-                                //   dotIncreasedColor: Color(0xffFFB500),
-                                //   images: [
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //   ],
-                                // ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  //Drawn Path
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.69, top: 159.69),
-                    child: Image(
-                        image: AssetImage(
-                            "assests/categories_screen/Drawn_Path/Drawn_Path.png")),
-                  ),
-                  //COFFEE Text
-                  Container(
-                    padding: EdgeInsets.fromLTRB(15, 167, 15, 11),
-                    child: Text(
-                      "COFFEE",
-                      style: TextStyle(
-                        fontFamily: "Antipasto Pro",
-                        fontSize: 62,
-                        color: Color.fromRGBO(244, 239, 227, 1),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Stack(
-                children: [
-                  //Starbucks Logo, Starbucks Watermark
-                  Container(
-                    // width: screenWidth,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          //-------Empty Space-----------
-                          child: Container(),
-                          flex: 1,
-                        ),
-                        //-------Starbucks Logo--------
-                        Image(
-                          image: AssetImage(
-                              "assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png"),
-                        ),
-                        //-------Empty Space-----------
-                        Expanded(
-                          flex: 8,
-                          child: Container(),
-                        ),
-                        //-------Starbucks Watermark--------
-                        Image(
-                          image: AssetImage(
-                              "assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  //Coffee Card, Coffee Card Watermark
-                  Positioned(
-                    top: 251.17,
-                    left: 40.87,
-                    child: Stack(
-                      children: [
-                        Image(
-                            image: AssetImage(
-                                "assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png")),
-                        Positioned(
-                          top: 16.83,
-                          left: 27.13,
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 322,
-                                height: 444,
-                                child: Image(
-                                  image: AssetImage(
-                                      "assests/categories_screen/Coffee_Card/Coffee_Card.png"),
-                                ),
-                                // child: Carousel(
-                                //   indicatorBgPadding: 2.0,
-                                //   autoplay: false,
-                                //   dotBgColor: Colors.transparent,
-                                //   boxFit: BoxFit.fitHeight,
-                                //   dotIncreasedColor: Color(0xffFFB500),
-                                //   images: [
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //     Image.asset(
-                                //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
-                                //   ],
-                                // ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  //Drawn Path
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.69, top: 159.69),
-                    child: Image(
-                        image: AssetImage(
-                            "assests/categories_screen/Drawn_Path/Drawn_Path.png")),
-                  ),
-                  //COFFEE Text
-                  Container(
-                    padding: EdgeInsets.fromLTRB(15, 167, 15, 11),
-                    child: Text(
-                      "COFFEE",
-                      style: TextStyle(
-                        fontFamily: "Antipasto Pro",
-                        fontSize: 62,
-                        color: Color.fromRGBO(244, 239, 227, 1),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              Burger(),
+              IceCream(),
             ],
           ),
           //Dock
@@ -381,6 +180,209 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
           ),
         ],
       ),
+    );
+  }
+}
+
+
+class Burger extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        //Starbucks Logo, Starbucks Watermark
+        Container(
+          // width: screenWidth,
+          child: Column(
+            children: [
+              Expanded(
+                //-------Empty Space-----------
+                child: Container(),
+                flex: 1,
+              ),
+              //-------Starbucks Logo--------
+              Image(
+                image: AssetImage(
+                    "assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png"),
+              ),
+              //-------Empty Space-----------
+              Expanded(
+                flex: 8,
+                child: Container(),
+              ),
+              //-------Starbucks Watermark--------
+              Image(
+                image: AssetImage(
+                    "assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png"),
+              ),
+            ],
+          ),
+        ),
+        //Coffee Card, Coffee Card Watermark
+        Positioned(
+          top: 251.17,
+          left: 40.87,
+          child: Stack(
+            children: [
+              Image(
+                  image: AssetImage(
+                      "assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png")),
+              Positioned(
+                top: 16.83,
+                left: 27.13,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 322,
+                      height: 444,
+                      child: Image(
+                        image: AssetImage(
+                            "assests/categories_screen/Coffee_Card/Coffee_Card.png"),
+                      ),
+                      // child: Carousel(
+                      //   indicatorBgPadding: 2.0,
+                      //   autoplay: false,
+                      //   dotBgColor: Colors.transparent,
+                      //   boxFit: BoxFit.fitHeight,
+                      //   dotIncreasedColor: Color(0xffFFB500),
+                      //   images: [
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //   ],
+                      // ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        //Drawn Path
+        Padding(
+          padding: const EdgeInsets.only(left: 8.69, top: 159.69),
+          child: Image(
+              image: AssetImage(
+                  "assests/categories_screen/Drawn_Path/Drawn_Path.png")),
+        ),
+        //COFFEE Text
+        Container(
+          padding: EdgeInsets.fromLTRB(15, 167, 15, 11),
+          child: Text(
+            "COFFEE",
+            style: TextStyle(
+              fontFamily: "Antipasto Pro",
+              fontSize: 62,
+              color: Color.fromRGBO(244, 239, 227, 1),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IceCream extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        //Starbucks Logo, Starbucks Watermark
+        Container(
+          // width: screenWidth,
+          child: Column(
+            children: [
+              Expanded(
+                //-------Empty Space-----------
+                child: Container(),
+                flex: 1,
+              ),
+              //-------Starbucks Logo--------
+              Image(
+                image: AssetImage(
+                    "assests/categories_screen/Starbucks_Logo/Starbucks_Logo.png"),
+              ),
+              //-------Empty Space-----------
+              Expanded(
+                flex: 8,
+                child: Container(),
+              ),
+              //-------Starbucks Watermark--------
+              Image(
+                image: AssetImage(
+                    "assests/categories_screen/STARBUCKS_Watermark/STARBUCKS_Watermark.png"),
+              ),
+            ],
+          ),
+        ),
+        //Coffee Card, Coffee Card Watermark
+        Positioned(
+          top: 251.17,
+          left: 40.87,
+          child: Stack(
+            children: [
+              Image(
+                  image: AssetImage(
+                      "assests/categories_screen/Coffee_Card_Watermark/Coffee_Card_Watermark.png")),
+              Positioned(
+                top: 16.83,
+                left: 27.13,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 322,
+                      height: 444,
+                      child: Image(
+                        image: AssetImage(
+                            "assests/categories_screen/Coffee_Card/Coffee_Card.png"),
+                      ),
+                      // child: Carousel(
+                      //   indicatorBgPadding: 2.0,
+                      //   autoplay: false,
+                      //   dotBgColor: Colors.transparent,
+                      //   boxFit: BoxFit.fitHeight,
+                      //   dotIncreasedColor: Color(0xffFFB500),
+                      //   images: [
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //     Image.asset(
+                      //         'assests/categories_screen/Coffee_Card/Coffee_Card.png'),
+                      //   ],
+                      // ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        //Drawn Path
+        Padding(
+          padding: const EdgeInsets.only(left: 8.69, top: 159.69),
+          child: Image(
+              image: AssetImage(
+                  "assests/categories_screen/Drawn_Path/Drawn_Path.png")),
+        ),
+        //COFFEE Text
+        Container(
+          padding: EdgeInsets.fromLTRB(15, 167, 15, 11),
+          child: Text(
+            "COFFEE",
+            style: TextStyle(
+              fontFamily: "Antipasto Pro",
+              fontSize: 62,
+              color: Color.fromRGBO(244, 239, 227, 1),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
